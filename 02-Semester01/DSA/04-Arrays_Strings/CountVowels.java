02f6a6ff904ac049438d1aa8b970bde166bf3dc7
Nitheesh C <nitheeshc@protonmail.com>
2025-09-16 05:54:10 +0000

    404 added

--- 02-Semester01/DSA/04-Arrays_Strings/CountVowels.java
+++ 02-Semester01/DSA/04-Arrays_Strings/CountVowels.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class CountVowels {
     public static void main(String[] args) {
-        String name = "Alice";
+        Scanner inp = new Scanner(System.in);
+        String name = inp.nextLine();
         int count=0;
         for (char c: name.toLowerCase().toCharArray()) {
             if ("aeiou".indexOf(c) != -1) count++;
--- 02-Semester01/DSA/04-Arrays_Strings/StringOps.java
+++ 02-Semester01/DSA/04-Arrays_Strings/StringOps.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
+
 public class StringOps {
     public static void main(String[] args) {
-        String s = "Apple";
+        Scanner inp = new Scanner(System.in);
+        String s = inp.nextLine();
         int len=0;
-        for(char _: s.toCharArray()) {
+        for(char a: s.toCharArray()) {
             len++;
         }
         System.out.println(len);
 
-        for(int i=len-1;i>=0;i--) System.out.print(s.charAt(i));
+        for(int i=len-1;i>=0;i--) {
+            System.out.print(s.charAt(i));
+        }
         System.out.println();
 
         String copy="";
-        for(int i=0;i<len;i++) copy += s.charAt(i);
+        for(int i=0;i<len;i++) {
+            copy += s.charAt(i);
+        }
         System.out.println(copy);
     }
 }
