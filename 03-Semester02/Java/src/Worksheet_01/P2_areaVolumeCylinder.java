2c5f2b0e37b71ae0a944a4623093bbf7d3b193e4
nitheeshmk41 <nitheeshmk5@gmail.com>
2026-01-05 21:27:24 +0530

    Worksheet01 p2,p3

---
+++ 03-Semester02/Java/src/Worksheet_01/P2_areaVolumeCylinder.java
@@ -0,0 +1,21 @@
+package Worksheet_01;
+
+import java.util.Scanner;
+
+public class P2_areaVolumeCylinder {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        float PI = 3.14f;
+        System.out.print("Enter radius : ");
+        double rad = inp.nextDouble();
+        System.out.print("Enter length : ");
+        double len = inp.nextDouble();
+
+        double area = PI * rad * rad;
+        double vol = area * len;
+
+        System.out.printf("Area is : %.2f%n" ,  area);
+        System.out.println("Volume is : "+ vol);
+    }
+}
---
+++ 03-Semester02/Java/src/Worksheet_01/P3_BMI.java
@@ -0,0 +1,29 @@
+package Worksheet_01;
+
+import java.util.Scanner;
+
+public class P3_BMI {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Gender (M / F) : ");
+        char gender = inp.next().charAt(0);
+        if(gender != 'M' && gender != 'F' ){
+            System.out.print("Enter valid ! Gender (M / F) : ");
+            gender = inp.next().charAt(0);
+        }
+
+        System.out.print("Enter ur weight in Kgs : ");
+        double weight = inp.nextDouble();
+        System.out.print("Enter ur Height in cms : ");
+        double height = inp.nextDouble();
+
+        double bmi = weight / (height * height);
+
+        if( (bmi >= 27.8 && gender == 'M') || (bmi >= 25 && gender == 'F')){
+            System.out.println("Obesee !!");
+        }else{
+            System.out.println("Normal");
+        }
+    }
+}
