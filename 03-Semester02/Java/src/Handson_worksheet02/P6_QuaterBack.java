fc9c5d56cb1a9e17adf320b63df6c86df3351ab3
25mx336 <25mx336@MCA.PSG>
2026-02-10 11:01:02 +0530

    add: P6-HW2

---
+++ 03-Semester02/Java/src/Handson_worksheet02/P6_QuaterBack.java
@@ -0,0 +1,17 @@
+package Handson_worksheet02;
+
+import java.util.Scanner;
+
+public class P6_QuaterBack {
+    static void n(String n){
+        n = "Samppple";
+    }
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        StringBuilder a = new StringBuilder();
+        String ans = "abs";
+        System.out.println(ans);
+        n(ans);
+        System.out.println(ans);
+    }
+}
